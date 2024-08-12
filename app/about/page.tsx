b8d5c2749b56d0aed84afd6c79d3c22cc4b54863
Saif <Saifsep17@gmail.com>
2024-08-12 23:08:49 +0530

    Created about page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Our Story - ZenGravitas",
+  description: "...",
+};
+
+import React from "react";
+
+const page = () => {
+  return (
+    <main className="text-center items-center">
+      <h1 className="text-black font-bold text-4xl">Know Our Story</h1>
+    </main>
+  );
+};
+
+export default page;

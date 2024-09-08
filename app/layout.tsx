cdb3b73d34bfdb70e0c4089ed9399c27ac59f687
ThunderBird07 <70371837+ThunderBird07@users.noreply.github.com>
2024-09-08 23:30:44 +0530

    Favicon Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${inter.variable} bg-gray-50 font-inter tracking-tight antialiased flex flex-row justify-center`}
       >
